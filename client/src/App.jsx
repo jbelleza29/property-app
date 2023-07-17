27ab1237daaa25b1ba3e99a7d96d075f72075a76
Pritz Belleza <pritz.belleza@gmail.com>
2023-07-17 15:24:24 +0800

    add client side querying

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
+import { gql } from "apollo-boost";
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
 import './App.css'
 
+const USER_PROPERTY_DETAILS = gql`
+  query userInfo($input: String!){
+    search(input: $input) {
+      firstName
+      lastName
+      properties {
+        city
+        state
+        street
+        zip
+        rent
+      }
+    }
+  }
+`
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState('');
+  const onChangeInput = (value) => {
+    setSearchValue(value)
+  };
+
+  const onClickSearch = () => {
+    userPropertyDetails();
+  };
+
+  const [userPropertyDetails, { loading, error, data }] = useLazyQuery(USER_PROPERTY_DETAILS, {
+    variables: { input: searchValue }
+  });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="search-container">
+          <label htmlFor='search-input'>Search</label>
+          <input id='search-input' value={searchValue} onChange={(e) => onChangeInput(e.target.value)}></input>
+          <button onClick={onClickSearch}>Search</button>
+        </div>
+        <ul className="search-results">
+          {data?.search?.map((item, index) => {
+            return <li key={`user-info-${index}`}>
+              <p>First Name: {item.firstName}</p>
+              <p>Last Name: {item.lastName}</p>
+              <h3>Properties</h3>
+              {item?.properties?.map((property, index2) => {
+                return <div key={`user-info-${index}-property-${index2}`}>
+                  <p>City: {property.city}</p>
+                  <p>State: {property.state}</p>
+                  <p>Street: {property.street}</p>
+                  <p>Zip: {property.zip}</p>
+                  <p>Rent: ${property.rent}</p>
+                </div>
+              })}
+            </li>
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
